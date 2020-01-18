c60103f24c3db56618d3b9fb7e4fe256e90a2f32
Akash Minz <akashminz.299@gmail.com>
2020-01-18 13:50:55 +0530

    New look and optimized

--- main.py
+++ main.py
@@ -5,14 +5,10 @@ import eel
 eel.init('web')
 
 #Exposing encrypt function so that js can communicate with python 
-@eel.expose
-def encrypt(USER_TXT,USER_KEY):
-    #Global
-    ENCRYPTED_TXT=''
+
+def createKey(USER_KEY):
     TEMP_KEY=0
     KEY=0
-    
-    #Calculating temporary key
     for char in USER_KEY:
         TEMP_KEY=TEMP_KEY+ord(char)
     
@@ -20,32 +16,27 @@ def encrypt(USER_TXT,USER_KEY):
     while(TEMP_KEY!=0):
         KEY=KEY+TEMP_KEY%10
         TEMP_KEY=TEMP_KEY//10
+    return KEY
 
+@eel.expose
+def encrypt(USER_TXT,USER_KEY):
+    #Global variable 
+    ENCRYPTED_TXT=''
+    
     #Encrypting user text using the key
     for char in USER_TXT:
-        ENCRYPTED_TXT=ENCRYPTED_TXT+chr(ord(char)+int(KEY))
+        ENCRYPTED_TXT=ENCRYPTED_TXT+chr(ord(char)+createKey(USER_KEY))
     return ENCRYPTED_TXT
 
 #Exposing decrypt function so that js can communicate with python 
 @eel.expose
 def decrypt(USER_TXT,USER_KEY):
-    #Global
+    #Global Variables
     DECRYPTED_TXT=''
-    TEMP_KEY=0
-    KEY=0
 
-    #Calculating temporary key
-    for char in USER_KEY:
-        TEMP_KEY=TEMP_KEY+ord(char)
-    
-    #Calculating the key for decrypting using temporary key
-    while(TEMP_KEY!=0):
-        KEY=KEY+TEMP_KEY%10
-        TEMP_KEY=TEMP_KEY//10
-    
     #Decrypting user text using the key
     for char in USER_TXT:
-        DECRYPTED_TXT=DECRYPTED_TXT+chr(ord(char)-KEY)
+        DECRYPTED_TXT=DECRYPTED_TXT+chr(ord(char)-createKey(USER_KEY))
     return DECRYPTED_TXT
 
 #Start eel from the index file
