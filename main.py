1214a57594ff92efdab5e43198f71b7a263b4c33
Akash Minz <akashminz.299@gmail.com>
2020-01-18 12:18:21 +0530

    Comments added

--- main.py
+++ main.py
@@ -1,44 +1,55 @@
+#Library for creating desktop application using web development tools
 import eel
 
+#Project folder that contins html, css, js files 
 eel.init('web')
 
+#Start eel from the index file
+eel.start('index.html',size=(1000,600))
+
+#Exposing encrypt function so that js can communicate with python 
 @eel.expose
 def encrypt(USER_TXT,USER_KEY):
+    #Global
     ENCRYPTED_TXT=''
     TEMP_KEY=0
     KEY=0
-
     
+    #Calculating temporary key
     for char in USER_KEY:
         TEMP_KEY=TEMP_KEY+ord(char)
-    print(TEMP_KEY)
-    print(type(TEMP_KEY))
     
+    #Calculating the key for encrypting using temporary key
     while(TEMP_KEY!=0):
         KEY=KEY+TEMP_KEY%10
         TEMP_KEY=TEMP_KEY//10
-    print(KEY)
 
+    #Encrypting user text using the key
     for char in USER_TXT:
         ENCRYPTED_TXT=ENCRYPTED_TXT+chr(ord(char)+int(KEY))
     return ENCRYPTED_TXT
 
+#Exposing decrypt function so that js can communicate with python 
 @eel.expose
 def decrypt(USER_TXT,USER_KEY):
+    #Global
     DECRYPTED_TXT=''
     TEMP_KEY=0
     KEY=0
+
+    #Calculating temporary key
     for char in USER_KEY:
         TEMP_KEY=TEMP_KEY+ord(char)
     print(TEMP_KEY)
     print(type(TEMP_KEY))
     
+    #Calculating the key for decrypting using temporary key
     while(TEMP_KEY!=0):
         KEY=KEY+TEMP_KEY%10
         TEMP_KEY=TEMP_KEY//10
-    print(KEY)
+    
+    #Decrypting user text using the key
     for char in USER_TXT:
         DECRYPTED_TXT=DECRYPTED_TXT+chr(ord(char)-KEY)
     return DECRYPTED_TXT
 
-eel.start('index.html',size=(1000,600))
